29580167e9c3a1894b8ddad6652bcbaf889d34c6
Roberto Mendez <roberto.mendez1983@gmail.com>
2022-04-03 14:00:24 +0200

    Sending a list

--- src/main/java/com/example/spring_initializr/HelloWorldController.java
+++ src/main/java/com/example/spring_initializr/HelloWorldController.java
@@ -3,12 +3,19 @@ package com.example.spring_initializr;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class HelloWorldController {
 
     @RequestMapping("/")
     public String index() {
         return "Hello World!";
+    }
+
+    @RequestMapping("/list")
+    public List<String> hello() {
+        return List.of("Hello", "World");
     }
 
 }
